c28142d8539b022b02e850708a86038b0c1a5648
bytecode20 <36814539+bytecode20@users.noreply.github.com>
2022-11-08 11:13:58 +0530

    Create AlphabetPallindrome.cpp

---
+++ Pattern Problems/AlphabetPallindrome.cpp
@@ -0,0 +1,54 @@
+Input: 4
+Output:
+   A
+  ABA
+ ABCBA
+ABCDCBA
+
+Code:
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                cout<<" ";
+            }
+            
+            for(int j=0;j<=i;j++)
+            {
+                cout<<char('A'+j);
+            }
+            for(int j=0;j<i;j++)
+            {
+                cout<<char('A'+i-j-1);
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
