45c1bcb29c70af38622bc6ba31e1cf7e060593ab
bytecode20 <36814539+bytecode20@users.noreply.github.com>
2022-11-14 11:56:37 +0530

    Create Print first n Fibonacci Numbers.cpp

---
+++ Arrays/Print first n Fibonacci Numbers.cpp
@@ -0,0 +1,72 @@
+Given a number N, find the first N Fibonacci numbers. The first two number of the series are 1 and 1.
+
+Example 1:
+
+Input:
+N = 5
+Output: 1 1 2 3 5
+Example 2:
+
+Input:
+N = 7
+Output: 1 1 2 3 5 8 13
+  
+  
+  Code:::::
+
+
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        //code here
+         vector<long long> a;
+         a.push_back(1);
+         if(n==1) return a;
+        a.push_back(1);    
+        int i=1;
+        n=n-2;
+        while(n--)
+        {
+            a.push_back(a[i]+a[i-1]);
+            i++;
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
