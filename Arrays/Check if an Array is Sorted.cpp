f3260f9ebe5f3a7722fe36bbbbb244b54e3f36ed
bytecode20 <36814539+bytecode20@users.noreply.github.com>
2022-11-21 10:51:24 +0530

    Create Check if an Array is Sorted.cpp

---
+++ Arrays/Check if an Array is Sorted.cpp
@@ -0,0 +1,61 @@
+Given an array arr[] of size N, check if it is sorted in non-decreasing order or not. 
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 1
+Explanation: The given array is sorted.
+Example 2:
+
+
+
+Code:::::
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]<arr[i-1])
+            {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        bool ans = ob.arraySortedOrNot(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
